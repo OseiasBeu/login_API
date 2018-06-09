4a3704f0ef75f37816ea09c8f193e0eff70357ea
Oseias Beu <oseiasbeu@outlook.com>
2018-06-09 00:08:38 -0300

    Criar transporter

---
+++ src/modules/mailer.js
@@ -0,0 +1,19 @@
+const nodemailer = require('nodemailer');
+const path = require('path');
+const {host, port, user,pass}= require('../config/mail.json');
+const hbs = require('nodemailer-express-handlebars');
+const transport = nodemailer.createTransport({
+  host,
+  port,
+  auth: {user,pass},
+});
+
+
+// configuração do Handlebars
+transport.use('compile', hbs({
+  viewEngine: 'handlebars',
+  viewPath: path.resolve('./src/resources/mail/'), //onde vão ficar as views de templates de emails 
+  extName: '.html', //tipo da extensão do arquivo
+}))
+
+module.exports = transport;
